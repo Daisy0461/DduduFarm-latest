5059b1f709038fc772aa4bba77f86d30044572a7
jaejune Yun <jjune0427@naver.com>
2022-08-10 23:14:35 +0900

    FArmState Change 완성

--- Assets/02.Scripts/Scenes/3_Farm/Objects/Farm/FarmSelect/FarmState.cs
+++ Assets/02.Scripts/Scenes/3_Farm/Objects/Farm/FarmSelect/FarmState.cs
@@ -12,7 +12,10 @@ public class FarmState : MonoBehaviour, IPointerDownHandler
     private bool isPlanted = false;
     [SerializeField]
     private bool isSelected = false;
+    [SerializeField]
     private SpriteRenderer farmSpriteRanderer;
+    [SerializeField]
+    private SpriteRenderer childSpriteRanderer;
 
     [SerializeField] [Tooltip ("자기 부모의 FarmSelect를 넣으면 됌.")]
     private FarmSelect parentFarmSelect;
@@ -21,19 +24,21 @@ public class FarmState : MonoBehaviour, IPointerDownHandler
     [Header ("선택된 씨앗")]
     public GameObject CropKind;
 
-    [SerializeField]
-    private GameObject warningPopup;
     [HideInInspector]
     public CropSelectButton cropSelectButton;
     [SerializeField]
     private AudioSource plantSound;
     private SpriteRenderer SR;
 
+    private bool plantMode = false;
+
     void Start()
     {
         IM = ItemManager.Instance;
         farmPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-        farmSpriteRanderer = gameObject.GetComponent<SpriteRenderer>();
+        // farmSpriteRanderer = gameObject.GetComponent<SpriteRenderer>();
+        // childSpriteRanderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+        
         originColor = farmSpriteRanderer.color;
     }
 
@@ -46,28 +51,49 @@ public class FarmState : MonoBehaviour, IPointerDownHandler
  
     void OnCollisionEnter2D(Collision2D coll)   //작물이 들어올 때 작동 - 바꿔야하나?
     {   // 밭의 네모난 것과 작물이 충돌
-        if(coll.gameObject.tag == "Crop"){
-            farmSpriteRanderer.color =  originColor;
+        if(coll.gameObject.tag == "Crop"){      //이거 떄문에 점선이 사라진다.
             isPlanted = true;
+            if(plantMode == false){             //작물이 들어왔는데 지금은 작물을 심고있는 중이 아니다.
+                farmSpriteRanderer.color =  originColor;
+                childSpriteRanderer.color = originColor;     
+            }else{                              //작물이 들어왔으며 지금은 작물을 심고있는 중이다.
+                farmSpriteRanderer.color =  new Color(255/255f ,85/255f, 85/255f, 255/255f);
+                childSpriteRanderer.color = new Color(255/255f ,85/255f, 85/255f, 255/255f);
+            }
         }
     }
 
-    void OnCollisionExit2D(Collision2D coll){   //작물이 채집될 때 작동
+    void OnCollisionExit2D(Collision2D coll){   //작물이 채집될 때 작동 진화할때도 작동하네?
+    //instaiate를 하고 작물이 채집이 되니까 한번 더 실행을 한다.
         if(coll.gameObject.tag == "Crop"){
             isPlanted = false;
+            farmSpriteRanderer.color =  new Color(255/255f, 230/255f, 128/255f, 255/255f);
+            childSpriteRanderer.color = originColor;
         }
+
+        farmSpriteRanderer.color = originColor;
+        childSpriteRanderer.color = originColor;
+        gameObject.SetActive(false);
+        gameObject.SetActive(true);     //완결까지 채집을 하니 충돌이 없어서 색이 변하지 않는다.
+        ChangeFarmColor();
     }
 
     public void ChangeFarmColor(){      //여기에 색 변화하게
+        plantMode = true;
         if(!isPlanted){     //작물이 심어져 있지 않은 상태라면 아래를 실행한다.
             farmSpriteRanderer.color =  new Color(255/255f, 230/255f, 128/255f, 255/255f);
             isSelected = true;
+        } else{             //작물이 심어져있다면 빨갛게 표시한다.
+            farmSpriteRanderer.color =  new Color(255/255f ,85/255f, 85/255f, 255/255f);
+            childSpriteRanderer.color = new Color(255/255f ,85/255f, 85/255f, 255/255f);
         }
     }
 
     public void ReturnColor(){      //원래 색으로 변경
         farmSpriteRanderer.color = originColor;
+        childSpriteRanderer.color = originColor;
         isSelected = false;
+        plantMode = false;
     }
 
     public void OnPointerDown(PointerEventData eventData){      //밭 터치 액션
@@ -87,9 +113,6 @@ public class FarmState : MonoBehaviour, IPointerDownHandler
             //숫자 새로고침 해줘야함.
             cropSelectButton.ActiveCropBtn();
             
-        }else if(afterPushFunc && isSelected && isPlanted || afterPushFunc && isPlanted){     //꾹 누른 작업을 한 후 원래는 작물이 없었지만 심어서 생긴 경우 & 꾹 누른 후 원래 작물이 있는 경우
-            //팝업 뜨게 하기
-            warningPopup.SetActive(true);
         }
     }
 }
